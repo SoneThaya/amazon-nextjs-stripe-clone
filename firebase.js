611daa1a9b9ace65290cb7881e0b1fc29da73a5b
sone <sthaya@gmail.com>
2022-10-12 20:53:12 -0700

    setup google sign in

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCDo4fqIxBkxRBvahZMctSu1gRf92EFe0",
+  authDomain: "amzn-stripe-tailwind.firebaseapp.com",
+  projectId: "amzn-stripe-tailwind",
+  storageBucket: "amzn-stripe-tailwind.appspot.com",
+  messagingSenderId: "268609901183",
+  appId: "1:268609901183:web:4f98f91ccb2a925255654a",
+};
