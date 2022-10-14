28a144a6b0b79c2abe8c5fe2d4ef97bb0a26d515
sone <sthaya@gmail.com>
2022-10-13 20:20:58 -0700

    setup checkout product and basket slice

---
+++ src/pages/checkout.js
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React from "react";
+import { useSelector } from "react-redux";
+import CheckoutProduct from "../components/CheckoutProduct";
+import Header from "../components/Header";
+import { selectItems } from "../slices/basketSlice";
+
+const Checkout = () => {
+  const items = useSelector(selectItems);
+
+  return (
+    <div className="bg-gray-100">
+      <Header />
+
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* left */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0 ? "Your basket is empty" : "Shopping basket"}
+            </h1>
+
+            {items.map((item) => (
+              <CheckoutProduct
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* right */}
+        <div></div>
+      </main>
+    </div>
+  );
+};
+
+export default Checkout;
